db51582c599dc9c94aa9f02708a9b338371f7876
Bappy <bappy776@gmail.com>
2022-03-27 20:52:33 +1100

    Delete concourse-sh.sh

--- tasks/concourse-sh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-cd repo
-date +'%d/%m/%Y %H:%M:%S' > todays-date
-git add ./todays-date
-git config --global user.email $EMAIL
-git config --global user.name $GIT_AUTHOR_NAME
-git commit -m "Update date"
